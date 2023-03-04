1e930e1404c1eff5cad47757f04d60116e338ba8
Rafik El Hadi Houari <rehhouari@gmail.com>
2023-03-04 21:23:21 +0100

    up

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import 'virtual:windi.css';
+// load this middleware
+import 'pinecone-router-middleware-views'
+// load pinecone router
+import PineconeRouter from 'pinecone-router';
+// then load alpine.js
+import Alpine from 'alpinejs';
+// add the router as a plugin
+Alpine.plugin(PineconeRouter)
+// start alpine
+Alpine.start()
